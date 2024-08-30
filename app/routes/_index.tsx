e1b8b3e1f71c416902004de72922425305c53092
Per Ivar Nerseth <perivar@nerseth.com>
2024-08-30 19:39:34 +0200

    Add song table

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,34 +1,41 @@
 // app/routes/_index.tsx
 
+import { useEffect, useState } from "react";
 import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { Link, redirect, useLoaderData } from "@remix-run/react";
 import { isSessionValid } from "~/fb.sessions.server";
+import { db } from "~/firebase-service";
+import {
+  collection,
+  DocumentData,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import { Edit, MoreHorizontal, Trash2 } from "lucide-react";
 
 import { Button } from "~/components/ui/button";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
-import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "~/components/ui/select";
-import { ToastAction } from "~/components/ui/toast";
-import { useToast } from "~/components/ui/use-toast";
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "ChordMaster" },
+    { name: "description", content: "Sheet music, chords and tabs" },
   ];
 };
 
@@ -40,21 +47,64 @@ export async function loader({ request }: LoaderFunctionArgs) {
     const decodedClaims = userSession?.decodedClaims;
     return { decodedClaims };
   } else {
-    return null;
-    // throw redirect("/login", {
-    //   statusText: (userSession?.error as Error)?.message,
-    // });
+    throw redirect("/login", {
+      statusText: (userSession?.error as Error)?.message,
+    });
   }
 }
 
 export default function Index() {
-  const { toast } = useToast();
   const loaderData = useLoaderData<typeof loader>();
+  const [songList, setSongList] = useState<
+    {
+      id: string;
+      song: DocumentData;
+    }[]
+  >();
+
+  useEffect(() => {
+    const getData = async () => {
+      const userId = loaderData.decodedClaims?.uid;
+
+      const songsQuery = query(
+        collection(db, "songs"),
+        where("user.uid", "==", userId)
+      );
+
+      const songsSnapshots = await getDocs(songsQuery);
+
+      if (songsSnapshots && songsSnapshots.size > 0) {
+        console.log(`Found ${songsSnapshots.size} Songs ...`);
+
+        const songs: { id: string; song: DocumentData }[] = [];
+        songsSnapshots.forEach(songSnapshot => {
+          const data = songSnapshot.data();
+          songs.push({ id: songSnapshot.id, song: data });
+        });
+        setSongList(songs);
+      }
+    };
+    getData();
+  }, [loaderData.decodedClaims]);
+
+  const handleEdit = async (id: number) => {
+    console.log(`Edit song with id: ${id}`);
+    alert(`Edit song with id: ${id}`);
+
+    // Implement edit functionality here
+  };
+
+  const handleDelete = async (id: number) => {
+    console.log(`Delete song with id: ${id}`);
+    alert(`Delete song with id: ${id}`);
+
+    // Implement delete functionality here
+  };
 
   return (
-    <section className="flex min-h-screen w-full flex-col">
-      {loaderData?.decodedClaims?.email && (
-        <div className="mt-2 flex flex-col items-center gap-2 text-sm text-gray-500 dark:text-gray-400">
+    <>
+      {loaderData.decodedClaims?.email && (
+        <div className="mt-5 flex flex-col items-center gap-2 text-sm text-gray-500 dark:text-gray-400">
           <div className="grid grid-cols-2 gap-2">
             <div>You are logged in as:</div>
             <div className="text-blue-600">
@@ -69,104 +119,87 @@ export default function Index() {
           </div>
         </div>
       )}
-      <div className="container flex flex-1 justify-center overflow-x-hidden p-4 md:px-6">
-        <div className="flex flex-col items-center space-y-4 p-4 text-center md:w-1/2">
-          <h1 className="text-3xl font-bold tracking-tighter md:text-4xl">
-            A{" "}
-            <span className="bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 bg-clip-text font-extrabold text-transparent">
-              Simple Starter
-            </span>{" "}
-            For Remix and Shadcn-ui
-          </h1>
 
-          <div className="font-sans">
-            <h1 className="text-2xl font-bold tracking-tighter md:text-3xl">
-              Welcome to Remix
-            </h1>
-            <ul className="mt-4 list-disc">
-              <li>
-                <Link
-                  className="hover:underline"
-                  target="_blank"
-                  to="https://remix.run/start/quickstart"
-                  rel="noreferrer">
-                  5m Quick Start
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="hover:underline"
-                  target="_blank"
-                  to="https://remix.run/start/tutorial"
-                  rel="noreferrer">
-                  30m Tutorial
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="hover:underline"
-                  target="_blank"
-                  to="https://remix.run/docs"
-                  rel="noreferrer">
-                  Remix Docs
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <Button
-              variant="outline"
-              onClick={() => {
-                toast({
-                  variant: "destructive",
-                  title: "Uh oh! Something went wrong.",
-                  description: "There was a problem with your request.",
-                  action: (
-                    <ToastAction altText="Try again">Try again</ToastAction>
-                  ),
-                });
-              }}>
-              Show Error Toast
-            </Button>
-          </div>
-
-          <Card className="w-[350px]">
-            <CardHeader>
-              <CardTitle>Dummy Card Example</CardTitle>
-              <CardDescription>Describe the function here.</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <form>
-                <div className="grid w-full items-center gap-4">
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="name">Name</Label>
-                    <Input id="name" placeholder="Name" />
-                  </div>
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="framework">Framework</Label>
-                    <Select>
-                      <SelectTrigger id="framework">
-                        <SelectValue placeholder="Select" />
-                      </SelectTrigger>
-                      <SelectContent position="popper">
-                        <SelectItem value="next">Next.js</SelectItem>
-                        <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                        <SelectItem value="astro">Astro</SelectItem>
-                        <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </div>
-                </div>
-              </form>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-              <Button variant="outline">Cancel</Button>
-              <Button>Deploy</Button>
-            </CardFooter>
-          </Card>
-        </div>
+      <div className="container mx-auto py-10">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="md:w-2/5">Song</TableHead>
+              <TableHead className="hidden md:table-cell">Artist</TableHead>
+              <TableHead className="hidden md:table-cell">Edit</TableHead>
+              <TableHead className="hidden md:table-cell">Delete</TableHead>
+              <TableHead className="md:hidden">Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {songList?.map((data, i) => {
+              const song = data.song;
+              return (
+                <TableRow key={i}>
+                  <TableCell className="md:hidden">
+                    <div className="flex items-center justify-between">
+                      <div>
+                        <div className="font-medium">{song.title}</div>
+                        <div className="text-sm text-muted-foreground">
+                          {song.artist.name}
+                        </div>
+                      </div>
+                    </div>
+                  </TableCell>
+                  <TableCell className="md:hidden">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="size-8 p-0">
+                          <span className="sr-only">Open menu</span>
+                          <MoreHorizontal className="size-4" />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem>Go To Artist</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem onClick={() => handleEdit(song.id)}>
+                          <Edit className="mr-2 size-4" />
+                          Edit
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem onClick={() => handleDelete(song.id)}>
+                          <Trash2 className="mr-2 size-4" />
+                          Delete
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                  <TableCell className="hidden font-medium md:table-cell">
+                    {song.title}
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    {song.artist.name}
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    <Button
+                      variant="ghost"
+                      size="sm"
+                      onClick={() => handleEdit(song.id)}>
+                      <Edit className="size-4" />
+                      <span className="sr-only">Edit</span>
+                    </Button>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    <Button
+                      variant="ghost"
+                      size="sm"
+                      onClick={() => handleDelete(song.id)}>
+                      <Trash2 className="size-4" />
+                      <span className="sr-only">Delete</span>
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
       </div>
-    </section>
+    </>
   );
 }
